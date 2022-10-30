9666fe1daa3f3184e598aad4bf966e4671a361be
311 <ismoilov92@gmail.com>
2022-10-30 23:57:32 +0300

    connect with telegram

---
+++ dispatcker/src/main/java/com/example/controller/TelegramBot.java
@@ -0,0 +1,33 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.name}")
+    private String botName;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var originalMessage  = update.getMessage();
+
+        System.out.println(originalMessage.getText());
+    }
+}
