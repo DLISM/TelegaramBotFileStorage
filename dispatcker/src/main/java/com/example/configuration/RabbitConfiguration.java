4daf6674b490296c0594668fba6a3046b09d8ed8
311 <ismoilov92@gmail.com>
2022-11-03 00:14:48 +0300

    added messages from tg to rabbitmq

---
+++ dispatcker/src/main/java/com/example/configuration/RabbitConfiguration.java
@@ -0,0 +1,39 @@
+package com.example.configuration;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static com.example.model.RabbitQueue.*;
+
+
+@Configuration
+public class RabbitConfiguration {
+    @Bean
+    public MessageConverter jsonMessageConvertor(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue textMessageQueue(){
+        return new Queue(TEXT_MESSAGE_UPDATE);
+    }
+
+    @Bean
+    public Queue docMessageQueue(){
+        return new Queue(DOC_MESSAGE_UPDATE);
+    }
+
+    @Bean
+    public Queue photoMessageQueue(){
+        return new Queue(PHOTO_MESSAGE_UPDATE);
+    }
+
+    @Bean
+    public Queue answerMessageQueue(){
+        return new Queue(ANSWER_MESSAGE);
+    }
+
+}
--- dispatcker/src/main/java/com/example/service/impl/UpdateProduceImpl.java
+++ dispatcker/src/main/java/com/example/service/impl/UpdateProduceImpl.java
@@ -2,14 +2,24 @@ package com.example.service.impl;
 
 import com.example.service.UpdateProducer;
 import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Service
 @Log4j
 public class UpdateProduceImpl implements UpdateProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public UpdateProduceImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
+
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
